5ce0ffaf1112b786ea667201fbeef86568178918
digpolo <digpolo@gmail.com>
2023-09-18 13:38:13 -0500

    CU-85ztze2fb - strucuture

---
+++ src/app/components/dishes.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+ const Dishes = () => {
+  return (
+    <div>dishes</div>
+  )
+}
+export default Dishes
---
+++ src/app/components/fruit.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+ const Fruit = () => {
+  return (
+    <div>F</div>
+  )
+}
+
+export default Fruit
