0f3d55c0bd2e43683766cde47631d4d03d45b343
digpolo <digpolo@gmail.com>
2023-09-18 15:47:32 -0500

    CU-85ztztvad - dishes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 import Header from './components/header'
 import NavBar from './components/nav-bar'
+import Dishes from './components/dishes'
 
 export default function Home() {
   return (
-    <div className='h-creen'>
-  
+    <div className='px-4'>
       <Header />
+      <Dishes />
     </div>
   )
 }
