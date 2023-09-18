f3abb67675be4e081a470ad4db4964d27ef00189
digpolo <digpolo@gmail.com>
2023-09-18 18:02:50 -0500

    CU-85ztztvbe - fruit

--- src/app/components/fruit.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
- const Fruit = () => {
-  return (
-    <div>F</div>
-  )
-}
-
-export default Fruit
--- src/components/dishes.tsx
+++ src/components/dishes.tsx
@@ -1,16 +1,16 @@
-import Image from 'next/image';
+import Image from 'next/image'
 import React from 'react'
-import Button from './button';
+import Button from './button'
 
 const Dishes = () => {
   return (
-    <div className='flex flex-col items-center gap-6 w-full'>
-      <h2>¿Cuantos tipos de frutas prefieres?</h2>
+    <div className='flex flex-col items-center justify-between h-screen my-10'>
+      <h2 className='py-4'>¿Cuantos tipos de frutas prefieres?</h2>
       <div className='flex flex-col items-center'>
         <Image
           src='/4dish.svg'
           alt='imagen de plato 4 puestos'
-          width={200} height={55}
+          width={150} height={55}
         />
         <p>4 tipos de fruta</p>
       </div>
@@ -18,7 +18,7 @@ const Dishes = () => {
         <Image
           src='/3dish.svg'
           alt='imagen de plato 4 puestos'
-          width={110} height={55}
+          width={90} height={55}
         />
         <p>4 tipos de fruta</p>
       </div>
@@ -30,7 +30,7 @@ const Dishes = () => {
         />
         <p>4 tipos de fruta</p>
       </div>
-      <div className='flex justify-center w-full '>
+      <div className='flex justify-center'>
       <Button label='REALIZAR PEDIDO' condition />
       </div>
     </div>
